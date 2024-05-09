540d7fc5c65ab110c25339d61f774f8dab65fe9e
Jonathas Meine <jonhy.meine@gmail.com>
2024-05-09 17:22:46 -0300

    feat: add spacing options based on TailwindCSS breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                sm: '40rem',
+                md: '48rem',
+                lg: '64rem',
+                xl: '80rem',
+                '2xl': '96rem',
+            },
+        },
     },
     plugins: [],
 };
