e9278e182318294b40926b3fda43b3201e8da99b
Jonathas Meine <jonhy.meine@gmail.com>
2024-07-15 20:33:08 -0300

    feat: add control bar

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,15 @@
 import '../index.css';
-import container_background from '../assets/images/backgrounds/mirage.jpeg';
+import containerBackground from '../assets/images/backgrounds/mirage.jpeg';
 import ItemPool from './ItemPool';
+import ControlBar from './ControlBar';
 
 function Container() {
     return (
         <main className="container mx-auto min-h-sm max-h-md relative overflow-hidden bg-slate-400 sm:mt-8">
-            <img className="min-h-sm blur object-cover scale-110" src={container_background} />
+            <img className="min-h-sm blur object-cover scale-110" src={containerBackground} />
             <div className="absolute inset-0 flex flex-col z-10">
                 <ItemPool />
-                <div className="block grow min-h-24"></div>
+                <ControlBar />
             </div>
         </main>
     );
