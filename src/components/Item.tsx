1f3d2063f5fcbff36e6705bd50dd0a2feb51d300
Jonathas Meine <jonhy.meine@gmail.com>
2024-07-15 20:30:48 -0300

    refactor: move skin image

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,11 +1,11 @@
 import '../index.css';
-import weapon_skin from '../assets/images//cases/case_1/nightwish.png';
+import weaponSkin from '../assets/images//cases/nightmare_case/nightwish.png';
 
 function Item() {
     return (
         <div className="w-full h-full">
             <div className="h-40 bg-gradient-to-b from-20% from-zinc-600 to-90% to-zinc-400">
-                <img className="w-full h-full object-cover border-b-8 border-red-600" src={weapon_skin} />
+                <img className="w-full h-full object-cover border-b-8 border-red-600" src={weaponSkin} />
             </div>
             <div>
                 <h2 className="text-white font-bold">AK-47</h2>
